0e9ad74170aaad2897303cec2a70e1038f8ec829
dremglob <stourangeau1@studentmail.nait.ca>
2015-10-27 16:46:30 -0600

    Security Bizniz

--- DMIT-2018-In-Class-A02-master/eRestaurant Demo/eRestaurant.Framework/BLL/Security/UserManager.cs
+++ DMIT-2018-In-Class-A02-master/eRestaurant Demo/eRestaurant.Framework/BLL/Security/UserManager.cs
@@ -1,4 +1,5 @@
-﻿using eRestaurant.Framework.DAL.Security;
+﻿using eRestaurant.Framework.DAL;
+using eRestaurant.Framework.DAL.Security;
 using eRestaurant.Framework.Entities.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -10,11 +11,70 @@ using System.Threading.Tasks;
 
 namespace eRestaurant.Framework.BLL.Security
 {
+
+
+    
+
+    
     public class UserManager : UserManager<ApplicationUser>
     {
-        public UserManager()
-            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
-        {
-        }
+        #region Constants
+    // Ideally, we should put some/all of these in the Web.config file underneath the <appSettings> element.
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        private const string STR_EMAIL_FORMAT = "{0}@eRestaurant.tba";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+    #endregion
+        #region Constructor(s)
+            public UserManager()
+                : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+            {
+            }
+        #endregion
+        #region Custom Methods
+            public void AddDefaultUsers()
+            {
+                using (var context = new RestaurantContext())
+                {
+                    var employees = from data in context.Waiters
+                                    where !data.ReleaseDate.HasValue // they are employed
+                                    select data;
+                    foreach (var person in employees)
+                    {
+                        // check if they exist as a user
+                        // if (NOT any of the users exist such that(=>) u(represents a human user) where the
+                        // user's WaiterID has a value AND the user's WaiterID matches this person's WaiterID)
+                        if (!Users.Any(u => u.WaiterID.HasValue && u.WaiterID.Value == person.WaiterID))
+                        {
+                            // Add them as a user..
+                            string userName = string.Format(STR_USERNAME_FORMAT,
+                                person.FirstName, person.LastName);
+                            var appUser = new ApplicationUser()
+                            {
+                                UserName = userName,
+                                Email = string.Format(STR_EMAIL_FORMAT, userName),
+                                WaiterID = person.WaiterID
+                            };
+                            // NOTE: The following line of code needs to use the this keyword in order to have
+                            // access to the Create() extension method.
+                            this.Create(appUser, STR_DEFAULT_PASSWORD);
+
+                        }
+                    }
+                }
+                // 2) Create a web-master user
+                if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+                {
+                    // Add the webmaster
+                    var webMasterAccount = new ApplicationUser()
+                    {
+                        UserName = STR_WEBMASTER_USERNAME,
+                        Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                    };
+                    this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+                }
+            }
+        #endregion
     }
+    
 }
